1848fb9b1cafefc4c7fa932148924002c8974f47
thomascswalker <thomascswalker@gmail.com>
2025-05-22 10:44:49 -0400

    convert game to lib

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,7 +1,13 @@
 #include "Platform/Windows/WindowsPlatform.h"
 
+#include <iostream>
+
 int main()
 {
 	auto Platform = WindowsPlatform();
+	while (true)
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
 	return 0;
 }
